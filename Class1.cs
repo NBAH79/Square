5e22cb6a30f2690af40c03cc0804b77372e06fce
nbah79 <pavlovskyivan.games@gmail.com>
2018-11-22 19:40:47 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Square {
+    public interface Shape {
+        double GetSquare();
+    }
+
+    public struct Vector {
+        public float x;
+        public float y;
+
+        //конструктора без параметров быть не может
+
+        public Vector(float _x, float _y) {
+            x = _x;
+            y = _y;
+        }
+
+        public Vector(Vector v) {
+            x = v.x;
+            y = v.y;
+        }
+
+        static public Vector operator -(Vector v1, Vector v2) {
+            Vector ret;
+            ret.x = v1.x - v2.x;
+            ret.y = v1.y - v2.y;
+            return ret;
+        }
+
+        public double Length() {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+
+    public class Circle : Shape {
+        private float radius;
+
+        //чтоб могли менять радиус в процессе, если надо
+        public float Radius {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        //приватный конструктор чтоб не создавали обьект без явного указания радиуса
+        private Circle() { }
+
+        public Circle(float _radius) {
+            Radius = _radius;
+        }
+
+        //реализация интерфейса
+        public double GetSquare() {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Triangle : Shape {
+
+
+
+        private Vector a, b, c;
+
+        //чтоб могли менять вершины в процессе, если надо
+        //структуры копируются а не ссылаются
+        public Vector A {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public Vector B {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public Vector C {
+            get { return c; }
+            set { c = value; }
+        }
+
+        //приватный конструктор чтоб не создавали без указания вершин
+        private Triangle() { }
+
+        public Triangle(ref Vector _A, ref Vector _B, ref Vector _C) {
+            A = _A;
+            B = _B;
+            C = _C;
+        }
+
+        public double GetSquare() {
+            Vector AB = new Vector(A - B);
+            Vector BC = new Vector(B - C);
+            Vector CA = new Vector(C - A);
+            double lab = AB.Length();
+            double lbc = BC.Length();
+            double lca = CA.Length();
+            double p = (lab + lbc + lca) / 2;
+            return Math.Sqrt(p * (p - lab) * (p - lbc) * (p - lca));
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test {
+    class Test {
+        //пока не инициализированы
+        Square.Circle c1;
+        Square.Triangle t1;
+
+        //эти скорее всего уже из файла с мешем загружены
+        Square.Vector a = new Square.Vector(0, 0);
+        Square.Vector b = new Square.Vector(1, 0);
+        Square.Vector c = new Square.Vector(0, 1);
+        
+        public Test() {
+            c1 = new Square.Circle(1.0f);
+            t1 = new Square.Triangle(ref a, ref b, ref c);
+        }
+
+        //в параметре указан интерфейс
+        public void CalculateSquareOfAShape(Square.Shape shape) {
+            Console.WriteLine("Square:{0}",shape.GetSquare());
+        }
+
+        //оба класса реализуют интерфейс и могут быть использованы
+        public void Run() {
+            CalculateSquareOfAShape(c1);
+            CalculateSquareOfAShape(t1);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Test test=new Test();
+            test.Run();
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
